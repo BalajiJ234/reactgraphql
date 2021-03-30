6288218e3171d5ae4b8a5cbadfbdc6a2759f3200
Balaji Janarthanan <balajijanarthanan1997@gmail.com>
2021-03-30 16:17:52 +0530

    creating-dynamic-queries

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
 import { useEffect, useState, useCallback } from "react";
 
 import github from "./db";
-import githubQuery from "./Query";
+import query from "./Query";
 import RepoInfo from "./RepoInfo";
 
 const App = () => {
   let [userName, setUserName] = useState("");
   let [repoList, setRepoList] = useState(null);
+  let [pageCount, setPageCount] = useState(10);
+  let [queryString, setQueryString] = useState("react");
+  let [totalCount, setTotalCount] = useState(null);
 
   const fetchData = useCallback(() => {
+    const queryText = JSON.stringify(query(pageCount, queryString));
     fetch(github.baseUrl, {
       method: "POST",
       headers: github.headers,
-      body: JSON.stringify(githubQuery),
+      body: queryText,
     })
       .then((res) => res.json())
       .then((data) => {
         const viewer = data.data.viewer;
         const repos = data.data.search.nodes;
+        const count = data.data.search.repositoryCount;
         setUserName(viewer.name);
         setRepoList(repos);
+        setTotalCount(count);
       })
       .catch((error) => {
         console.log(error);
@@ -35,6 +41,12 @@ const App = () => {
         <i className="bi bi-diagram-2-fill"></i> Repos
       </h1>
       <p>Hey there {userName}</p>
+      <p>
+        <b>Search For:</b>
+        {queryString} | <b>Items per page:</b>
+        {pageCount} |<b>Total repositories:</b>
+        {totalCount}
+      </p>
       {repoList && (
         <ul className="list-group list-group-flush">
           {repoList.map((repo) => (
