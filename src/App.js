6bdd422add600f1478382d3f724fde6ceef86123
Balaji Janarthanan <balajijanarthanan1997@gmail.com>
2021-03-30 14:24:59 +0530

    Creating and Fetch Using Callback Hook

--- src/App.js
+++ src/App.js
@@ -1,18 +1,12 @@
+import { useEffect, useState, useCallback } from "react";
+
 import github from "./db";
-import { useEffect } from "react";
+import githubQuery from "./Query";
 
 const App = () => {
-  useEffect(() => {
-    const githubQuery = {
-      query: `
-      { 
-        viewer { 
-          name
-        }
-      }
-      `,
-    };
+  let [userName, setUserName] = useState("");
 
+  const fetchData = useCallback(() => {
     fetch(github.baseUrl, {
       method: "POST",
       headers: github.headers,
@@ -20,18 +14,23 @@ const App = () => {
     })
       .then((res) => res.json())
       .then((data) => {
+        setUserName(data.data.viewer.name);
         console.log(data);
       })
       .catch((error) => {
         console.log(error);
       });
-    return () => {};
   }, []);
+
+  useEffect(() => {
+    fetchData();
+  }, [fetchData]);
   return (
     <div className="App container mt-5">
       <h1 className="text-primary">
         <i className="bi bi-diagram-2-fill"></i> Repos
       </h1>
+      <p>Hey there {userName}</p>
     </div>
   );
 };
